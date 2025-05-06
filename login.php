4afcf92f0c5aa7e5a11fa74facdf8028a1d1a016
Neymar103 <alifianmalvinno2008@gmail.com>
2025-05-06 14:58:26 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html lang="id">
 <head>
-    <title>Login Admin</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Eco.Treasure - Login</title>
+  <link rel="stylesheet" href="assets/css/login.css" />
 </head>
 <body>
-    <h2>Login</h2>
-    <form action="progres/login_proses.php" method="post">
+  <div class="container">
+    <div class="left">
+      <img src="assets/img/login2.png" alt="Ilustrasi Daur Ulang" />
+    </div>
+    <div class="right">
+      <div class="logo">ECO.TREA<span>SURE</span></div>
+      <h2>SELAMAT DATANG!</h2>
+        <form action="progres/login_proses.php" method="post">
       <input type="email" name="email" placeholder="Email" required><br><br>
       <input type="password" name="password" placeholder="Password" required><br><br>
-      <input type="submit" value="Login">
+      <button type="submit" class="btn">MASUK</button>
   </form>
+    </div>
+  </div>
+  <script src="assets/js/login.js"></script>
 </body>
 </html>
+
+
